0e28853bbd772eeeeddb23e872b0f6217fba9fb3
wwwrwww65-netizen <wwwrwww65@gmail.com>
2026-09-17 09:25:36 +0300

    feat: initialize MikroTik hotspot portal project
    - Add configuration file and env setup
    - Implement MikroTik login status endpoint
    - Create announcement system styles
    - Add project documentation and supporting assets

---
+++ config.js
@@ -0,0 +1,250 @@
+window.siteConfig = {
+    // 1. اسم الشبكة والعناوين والهوية
+    "siteName": "توشكا نت",
+    "network-name": "توشكا نت",
+    "subDecText": "لخدمات الإنترنت اللاسلكي",
+    "welcomeMessage": "مرحبا بكم في شبكة توشكا نت اللاسلكية",
+    "welcomeMessageV": true,
+    "erbV": false,
+
+    // 2. الشريط الإخباري والتراحيب
+    "news-line": "شبكة توشكا نت تهنئكم بذكرى المولد النبوي * اللهم صل  و سلم على محمد و آله و ارزقنا حسن إتباعه",
+    "news-line2": "",
+    "textSlider1": "شبكة توشكا نت تهنئكم بذكرى المولد النبوي * اللهم صل  و سلم على محمد و آله و ارزقنا حسن إتباعه",
+
+    // 3. أرقام خدمة العملاء والواتساب
+    "supportPhone": "714000499",
+    "service-number": "714000499",
+    "callPhone": "714000499",
+    "whatsappPhone": "967714000499",
+    "whatsappMsg": "مرحبا خدمة عملاء شبكة توشكا نت اللاسلكية",
+
+    // 4. خيارات سرعة الإنترنت (مطابقة لمشروع toshka السابق)
+    "fixedSpeedCardPrefixes": ["2", "3"],
+    "speed-select": 0,
+    "speed-var": "speed",
+    "speed-option": "",
+    "defaultSpeed": "",
+    "speedOptions": [
+        {
+            "label": "سرعة أفتراضية",
+            "value": "",
+            "selected": true,
+            "isDefault": true
+        },
+        {
+            "label": "سرعة منخفضة 512",
+            "value": "128K/512K",
+            "selected": false,
+            "isDefault": false
+        },
+        {
+            "label": "سرعة عادية 1 ميجا",
+            "value": "256K/1024K",
+            "selected": false,
+            "isDefault": false
+        },
+        {
+            "label": "سرعة متوسطة 2 ميجا",
+            "value": "512K/2048K",
+            "selected": false,
+            "isDefault": false
+        },
+        {
+            "label": "سرعة مفتوحة",
+            "value": "1024K/4096K",
+            "selected": false,
+            "isDefault": false
+        }
+    ],
+
+    // 5. صور السلايدر الإعلاني
+    "imageCount": "7",
+    "imageV": true,
+
+    // 6. باقات وأسعار الكروت (مطابقة تماماً لمشروع toshka السابق)
+    "packages": [
+        {
+            "price": "100 ريال",
+            "time": "3 ساعات",
+            "size": "180 ميجا",
+            "transfer": "180 ميجا",
+            "validity": "2 ايام",
+            "vl": "2 ايام"
+        },
+        {
+            "price": "150 ريال",
+            "time": "5 ساعات",
+            "size": "250 ميجا",
+            "transfer": "250 ميجا",
+            "validity": "4 ايام",
+            "vl": "4 ايام"
+        },
+        {
+            "price": "250 ريال",
+            "time": "3 ساعات",
+            "size": "650 ميجا",
+            "transfer": "650 ميجا",
+            "validity": "7 ايام",
+            "vl": "7 ايام"
+        },
+        {
+            "price": "500 ريال",
+            "time": "25 ساعة",
+            "size": "1400 ميجا",
+            "transfer": "1400 ميجا",
+            "validity": "15 يوم",
+            "vl": "15 يوم"
+        },
+        {
+            "price": "1000 ريال",
+            "time": "60 ساعة",
+            "size": "3000 ميجا",
+            "transfer": "3000 ميجا",
+            "validity": "30 يوم",
+            "vl": "30 يوم"
+        },
+        {
+            "price": "5000 ريال",
+            "time": "شهر",
+            "size": "13 جيجا",
+            "transfer": "13 جيجا",
+            "validity": "30 يوم",
+            "vl": "30 يوم"
+        }
+    ],
+    "profiles": [
+        {
+            "price": "100 ريال",
+            "time": "3 ساعات",
+            "transfer": "180 ميجا",
+            "validity": "2 ايام"
+        },
+        {
+            "price": "150 ريال",
+            "time": "5 ساعات",
+            "transfer": "250 ميجا",
+            "validity": "4 ايام"
+        },
+        {
+            "price": "250 ريال",
+            "time": "3 ساعات",
+            "transfer": "650 ميجا",
+            "validity": "7 ايام"
+        },
+        {
+            "price": "500 ريال",
+            "time": "25 ساعة",
+            "transfer": "1400 ميجا",
+            "validity": "15 يوم"
+        },
+        {
+            "price": "1000 ريال",
+            "time": "60 ساعة",
+            "transfer": "3000 ميجا",
+            "validity": "30 يوم"
+        },
+        {
+            "price": "5000 ريال",
+            "time": "شهر",
+            "transfer": "13 جيجا",
+            "validity": "30 يوم"
+        }
+    ],
+
+    // 7. العروض والخدمات
+    "offersTitle": "خدمات وعروض شبكة توشكا نت 🔥",
+    "offers": "أهلاً بك زائرنا العزيز 😍\n• اتصال سريع وخدمة مميزة وتقنية متجددة\n• شبكة توشكا نت ترحب بكم دائماً\n• نسعى لتقديم أفضل خدمة إنترنت سريعة ومتميزة",
+    "offersBadge": "عرض مجاني",
+    "offersBadgeV": true,
+    "offersBtnText": "📺 الاستراحة والبث المباشر",
+    "offersBtnUrl": "",
+    "offersBtnV": false,
+    "loan-button": false,
+    "loan-text": "أهلاً بك زائرنا في شبكة توشكا نت",
+
+    // 8. نقاط البيع والتوزيع (مطابقة تماماً لمشروع toshka السابق)
+    "salesPoints": [
+        "سوبر ماركت المستباني للمواد الغذائية والكماليات-المخرط سابقا امام مطعم الخير",
+        "المستباني للهواتف الذكية ومستلزماتها ولأأكترونيات-المخرط سابقا امام مطعم الخير",
+        "سوبر ماركت احمد عيدة مستباني مبنى المحكمة",
+        "عبد السلام قمري للمواد الغذائية والكماليات",
+        "البحرين-يحيى خمج",
+        "المعجمي للاكترونيات والهواتف الذكية امام المقوات",
+        "احمد علي الخميسي للمواد الغذائية والكماليات-امام مركز الرأفة",
+        "محمد قمري للدراجات النارية وقطع غيارها",
+        "الخدري للأكترونيات وخدمات الطاقة",
+        "ناصر خميسي للأكترونيات ومستلزماتها امام محطة العبيدي للغاز",
+        "بقالةعبدالله داحش بجانب عبد الراق خميسي للملابس",
+        "ناجي مستباني",
+        "وسيم خميسي مقابل صادق علم"
+    ],
+    "sell-points": [
+        {"name":" سوبر ماركت المستباني للمواد الغذائية والكماليات-المخرط سابقا امام مطعم الخير"},
+        {"name":" المستباني للهواتف الذكية ومستلزماتها ولأأكترونيات-المخرط سابقا امام مطعم الخير "},
+        {"name":" سوبر ماركت احمد عيدة مستباني مبنى المحكمة "},
+        {"name":" عبد السلام قمري للمواد الغذائية والكماليات   "},
+        {"name":" البحرين-يحيى خمج  "},
+        {"name":"المعجمي للاكترونيات والهواتف الذكية امام المقوات"},
+        {"name":" احمد علي الخميسي للمواد الغذائية والكماليات-امام مركز الرأفة   "},
+        {"name":" محمد قمري للدراجات النارية وقطع غيارها "},
+        {"name":" الخدري للأكترونيات وخدمات الطاقة "},
+        {"name":" ناصر خميسي للأكترونيات ومستلزماتها امام محطة العبيدي للغاز "},
+        {"name":"  بقالةعبدالله داحش بجانب عبد الراق خميسي للملابس "},
+        {"name":" ناجي مستباني "},
+        {"name":" وسيم خميسي مقابل صادق علم  "}
+    ],
+
+    // 9. روابط الترفيه واستراحة الشبكة
+    "estr": "",
+    "estrV": false,
+    "moba": "fiberlive/live.html",
+    "mobaV": true,
+    "redirect-to-esterahah": "",
+    "redirect-to-mobasher": "fiberlive/live.html",
+    "app-store-status-button": false,
+    "app-store-base-url": "",
+
+    // 10. إعدادات المصحف الشريف
+    "quranUrl": "",
+    "quranV": false,
+
+    // 11. إعدادات الكوكيز ونظام الحظر للأخطاء
+    "enableHotCookie": true,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 2,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+
+    // 12. إعدادات الإدخال ونوع تسجيل الدخول
+    "login-type": "user",
+    "input-autocomplete": "on",
+    "input-type": "tel",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-text-type-when": 0,
+    "price-button": true,
+    "sell-point-button": true,
+    "show-date-field": true
+};
+
+// كائنات التوافق
+window.hotspotConfig = Object.assign({}, window.siteConfig);
+window.appConfig = Object.assign({}, window.siteConfig);
+
+window.Config = function(cfg) {
+    if (cfg && typeof cfg === 'object') {
+        Object.assign(window.siteConfig, cfg);
+        Object.assign(window.hotspotConfig, cfg);
+        Object.assign(window.appConfig, cfg);
+    }
+};
